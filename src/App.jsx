eb8eb9f1efcc91448eb3487ca1fee25f890258ef
Masaki Suzuki <masakisuzuki.shkr@gmail.com>
2025-09-19 06:03:05 +0900

    react-routerのインストール、「タスク一覧画面」と「ゴミ箱画面」日長を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0);
+const router = createBrowserRouter([
+  {
+    element: <Outlet />,
+    children: [
+      {
+        // タスク一覧画面
+        path: "/",
+        element: (
+          <div>
+            <div>タスク一覧</div>
+            <Link to="trash">ゴミ箱へ</Link>
+          </div>
+        ),
+      },
+      {
+        // ゴミ箱画面
+        path: "/trash",
+        element: (
+          <div>
+            <div>ゴミ箱</div>
+            <Link to="/">タスク一覧へ</Link>
+          </div>
+        )
+      }
+    ]
+  }
+])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+const App = () => {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
